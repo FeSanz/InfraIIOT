1dcd1c42f1a8af8800ddfc6e024cc4eaee259922
FeSanz <felipe.antonio.condor@gmail.com>
2021-06-22 14:59:42 -0500

    BD correcccion
    Cambio de localhost

--- db_constants.php
+++ db_constants.php
@@ -1,11 +1,11 @@
 <?php 
-// define('DB_HOST', 'remotemysql.com');
-// define('DB_USER', 'VEEUbbBt3r');
-// define('DB_PASS', 'hNmZoOaTYA');
-// define('DB_NAME', 'VEEUbbBt3r');
+ define('DB_HOST', 'remotemysql.com');
+ define('DB_USER', 'VEEUbbBt3r');
+ define('DB_PASS', 'hNmZoOaTYA');
+ define('DB_NAME', 'VEEUbbBt3r');
  
- define('DB_HOST', 'localhost');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'infra');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'infra');
 
